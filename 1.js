fd96063908c56b9936aade62ef72c77af290b5a3
Nikita Rudometov <nikitarudometof@gmail.com>
2024-03-18 22:59:30 +0700

    fix: delete sandbox file

--- 1.js
+++
@@ -1,9 +0,0 @@
-
-function fromJSON(proto, json) {
-  const translating = new Promise((res) => {
-    res(JSON.parse(json));
-  }).then((res) => res(Object.create(res, proto)));
-  console.log(translating);
-}
-
-console.log(fromJSON('{ "a":10, "b":20, "c":30 }', {}));
